7754c8d26ae6daba3b5bcacc1a6e89e6fc6b1ed4
robiulhasanmumin <ot7mumin@gmail.com>
2025-01-28 16:08:50 +0600

    [Search]: search tearms comlete.

--- src/components/navigation/Search.jsx
+++ src/components/navigation/Search.jsx
@@ -1,26 +1,73 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { setSearchTerm } from "../../features/filter/filterSlice";
+
+// export default function Search(){
+//     const [input, setInput] = useState('');
+//     const dispatch = useDispatch();
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         dispatch(setSearchTerm(input))
+//     }
+//     return (
+//         <form onSubmit={handleSubmit} >
+//             <input
+//                 className="outline-none border-none mr-2"
+//                 type="search"
+//                 name="search"
+//                 placeholder="Search"
+//                 value={input}
+//                 onChange={(e)=>setInput(e.target.value)}
+//             />
+//         </form>
+//     )
+// }
+
+
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setSearchTerm } from "../../features/filter/filterSlice";
 
-export default function Search(){
-    const [input, setInput] = useState('');
+// Debounce হুক
+function useDebounce(value, delay) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+}
+
+export default function Search() {
+    const [input, setInput] = useState("");
     const dispatch = useDispatch();
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(setSearchTerm(input))
-    }
+
+    // ডেবাউন্স করা ইনপুট
+    const debouncedInput = useDebounce(input, 500);
+
+    useEffect(() => {
+        if (debouncedInput !== "") {
+            dispatch(setSearchTerm(debouncedInput));
+        }
+    }, [debouncedInput, dispatch]);
+
     return (
-        <form onSubmit={handleSubmit} >
+        <form onSubmit={(e) => e.preventDefault()}>
             <input
                 className="outline-none border-none mr-2"
                 type="search"
                 name="search"
                 placeholder="Search"
                 value={input}
-                onChange={(e)=>setInput(e.target.value)}
+                onChange={(e) => setInput(e.target.value)}
             />
         </form>
-    )
+    );
 }
-
-
